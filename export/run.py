eacf3b8d2901318239dcdbd40eac7307edb7a73e
Tyler Diderich <tyler@tylers-mbp.lan>
2022-06-15 09:47:17 -0500

    searches

--- export/run.py
+++ export/run.py
@@ -7,14 +7,24 @@ BASE_URL = "https://console.rumble.run/api/v1.0"
 
 
 def main():
-    search = "source:crowdstrike"
-    url = BASE_URL + "/export/org/assets.json"
-    assets = requests.get(
-        url,
-        headers=HEADERS,
-        params={"search": search},
-    )
-    print(assets.json())
+    searches = [
+        "_asset.protocol:ntp",
+        "_asset.protocol:ntp" "_asset.protocol:ntp and protocol:ntp and has:ntp.skew",
+        "_asset.protocol:tls AND tls.notAfterTS:<now",
+        'protocol:="mountd" and nfs.allowed:"%=*"',
+        "_asset.protocol:tls AND tls.notAfterTS:<6weeks",
+        "_asset.protocol:ntp and protocol:ntp and has:ntp.skew",
+        '(_asset.protocol:http AND not _asset.protocol:tls) AND ( html.inputs:"password:" OR last.html.inputs:"password:" OR has:http.head.wwwAuthenticate OR has:last.http.head.wwwAuthenticate)',
+        "_asset.protocol:kerberos and protocol:kerberos and kerberos.errorCode:68 and os:windows",
+        '_asset.protocol:ldap and protocol:ldap and (has:"ldap.isSynchronized" or has:"ldap.domainControllerFunctionality") and type:server and os:windows',
+    ]
+
+    for search in searches:
+        url = BASE_URL + "/export/org/assets.json?"
+        print(url)
+        assets = requests.get(url, headers=HEADERS, params={"search": search})
+        print(assets.status_code)
+        print(assets.json())
 
 
 if __name__ == "__main__":
