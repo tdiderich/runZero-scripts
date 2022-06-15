f7f26cb4d165ac65cf0a2ab23c43c29e0551bedc
Tyler Diderich <tyler@tylers-mbp.lan>
2022-06-15 10:47:59 -0500

    service support

--- export/README.md
+++ export/README.md
@@ -4,8 +4,9 @@ This is a basic script that runs a few searches + prints the results in JSON for
 
 ```
 $ python3 export/run.py
+Result count: 0
 [{'id': '68af77fb-c89b-4190-ba4e-528d4bfb4eaf', 'created_at': 1637609501, 'updated_at': 1637609501, 'organization_id': '183dc8c7-dfc1-4323-9ba9-7027c0b1a2ce', 'site_id': 'b19a58f7-0c91-4c2a-b6dd-05aea73fb772', 'alive': True, 'last_seen': 1583271230, 'first_seen': 1583271140, 'detected_by': 'arp', 'type': 'NAS', 'os_vendor': 'Synology', 'os_product': 'DSM', 'os_version': '', 'os': 'Synology Linux DSM', 'hw_vendor': 'Synology', 'hw_product': 'NAS', 'hw_version': '', 'hw': 'Synology NAS', 'addresses': ['192.168.0.40'], 'addresses_extra': ['10.100.100.40'], 'macs': ['00:11:32:5b:8b:b9'], 'mac_vendors': ['Synology Incorporated'], 'names': ['SURVEILLANCE'], 'tags': {}, 'tag_descriptions': None, 'domains': [], 'services': {'192.168.0.40/0/arp/': {'mac': '00:11:32:5b:8b:b9', 'macDateAdded': '2004-04-25', 'macVendor': 'Synology Incorporated', 'service.address': '192.168.0.40', 'service.id': '68af77fb-c89b-4190-ba4e-528d4bfb4eaf', 'service.port': '0', 'service.transport': 'arp', 'source': 'arp', 'ts': '1583271140'},
 ...
-more asset info
+more asset/service info
 ...
 ```
--- export/run.py
+++ export/run.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import urllib.parse
 
 RUMBLE_EXPORT_TOKEN = os.environ["RUMBLE_EXPORT_TOKEN"]
 HEADERS = {"Authorization": f"Bearer {RUMBLE_EXPORT_TOKEN}"}
@@ -8,21 +9,41 @@ BASE_URL = "https://console.rumble.run/api/v1.0"
 
 def main():
     searches = [
-        "_asset.protocol:ntp",
-        "_asset.protocol:ntp" "_asset.protocol:ntp and protocol:ntp and has:ntp.skew",
-        "_asset.protocol:tls AND tls.notAfterTS:<now",
-        'protocol:="mountd" and nfs.allowed:"%=*"',
-        "_asset.protocol:tls AND tls.notAfterTS:<6weeks",
-        "_asset.protocol:ntp and protocol:ntp and has:ntp.skew",
-        '(_asset.protocol:http AND not _asset.protocol:tls) AND ( html.inputs:"password:" OR last.html.inputs:"password:" OR has:http.head.wwwAuthenticate OR has:last.http.head.wwwAuthenticate)',
-        "_asset.protocol:kerberos and protocol:kerberos and kerberos.errorCode:68 and os:windows",
-        '_asset.protocol:ldap and protocol:ldap and (has:"ldap.isSynchronized" or has:"ldap.domainControllerFunctionality") and type:server and os:windows',
+        {"type": "assets", "search": "_asset.protocol:tls"},
+        {"type": "assets", "search": "_asset.protocol:ntp"},
+        {
+            "type": "services",
+            "search": "_asset.protocol:ntp and protocol:ntp and has:ntp.skew",
+        },
+        {"type": "asset", "search": "_asset.protocol:tls AND tls.notAfterTS:<now"},
+        {"type": "asset", "search": 'protocol:="mountd" and nfs.allowed:"%=*"'},
+        {"type": "asset", "search": "_asset.protocol:tls AND tls.notAfterTS:<6weeks"},
+        {
+            "type": "services",
+            "search": "_asset.protocol:ntp and protocol:ntp and has:ntp.skew",
+        },
+        {
+            "type": "services",
+            "search": '(_asset.protocol:http AND not _asset.protocol:tls) AND ( html.inputs:"password:" OR last.html.inputs:"password:" OR has:http.head.wwwAuthenticate OR has:last.http.head.wwwAuthenticate)',
+        },
+        {
+            "type": "services",
+            "search": "_asset.protocol:kerberos and protocol:kerberos and kerberos.errorCode:68 and os:windows",
+        },
+        {
+            "type": "services",
+            "search": '_asset.protocol:ldap and protocol:ldap and (has:"ldap.isSynchronized" or has:"ldap.domainControllerFunctionality") and type:server and os:windows',
+        },
     ]
 
     for search in searches:
-        url = BASE_URL + "/export/org/assets.json"
-        assets = requests.get(url, headers=HEADERS, params={"search": search})
-        print(assets.json())
+        if search["type"] == "assets":
+            url = BASE_URL + "/export/org/assets.json?"
+        else:
+            url = BASE_URL + "/export/org/services.json?"
+        data = requests.get(url, headers=HEADERS, params={"search": search["search"]})
+        print("Result count: " + str(len(data.json())))
+        print(data.json())
 
 
 if __name__ == "__main__":
