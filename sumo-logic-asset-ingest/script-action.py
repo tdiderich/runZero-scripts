b28cc8a49c87fe7bd58f64009370da6202d95618
Tyler Diderich <tyler@tylers-mbp.lan>
2023-03-22 16:13:14 -0500

    make script action  work with python2

--- sumo-logic-asset-ingest/script-action.py
+++ sumo-logic-asset-ingest/script-action.py
@@ -4,16 +4,16 @@ import requests
 import os
 
 # RUNZERO CONF
-RUNZERO_EXPORT_TOKEN = os.environ["RUNZERO_EXPORT_TOKEN"]
-HEADERS = {"Authorization": f"Bearer {RUNZERO_EXPORT_TOKEN}"}
-BASE_URL = "https://console.runZero.com/api/v1.0"
+RUNZERO_EXPORT_TOKEN = os.environ['RUNZERO_EXPORT_TOKEN']
+HEADERS = {'Authorization': 'Bearer ' + RUNZERO_EXPORT_TOKEN}
+BASE_URL = 'https://console.runZero.com/api/v1.0'
 
 
 def main():
     url = BASE_URL + '/export/org/assets.json'
     assets = requests.get(url, headers=HEADERS)
     for a in assets.json():
-        print(a)
+        print(json.dumps(a))
 
 
 if __name__ == '__main__':
