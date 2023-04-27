cbbdcde688a97b24e67e0d61269c7345512182c4
Tyler Diderich <tylerdiderich@gmail.com>
2023-04-27 10:29:20 -0500

    updated export script

--- export/README.md
+++ export/README.md
@@ -1,12 +1,11 @@
 # Export API
 
-This is a basic script that runs a few searches + prints the results in JSON format. You can update the endpoint to print in a different format and/or update the list of searches.
+This is a basic script that runs a few searches + prints the counts of the results. It shows how to filter on MAC, IP, and Name.
 
 ```
-$ python3 export/run.py
-Result count: 0
-[{'id': '68af77fb-c89b-4190-ba4e-528d4bfb4eaf', 'created_at': 1637609501, 'updated_at': 1637609501, 'organization_id': '183dc8c7-dfc1-4323-9ba9-7027c0b1a2ce', 'site_id': 'b19a58f7-0c91-4c2a-b6dd-05aea73fb772', 'alive': True, 'last_seen': 1583271230, 'first_seen': 1583271140, 'detected_by': 'arp', 'type': 'NAS', 'os_vendor': 'Synology', 'os_product': 'DSM', 'os_version': '', 'os': 'Synology Linux DSM', 'hw_vendor': 'Synology', 'hw_product': 'NAS', 'hw_version': '', 'hw': 'Synology NAS', 'addresses': ['192.168.0.40'], 'addresses_extra': ['10.100.100.40'], 'macs': ['00:11:32:5b:8b:b9'], 'mac_vendors': ['Synology Incorporated'], 'names': ['SURVEILLANCE'], 'tags': {}, 'tag_descriptions': None, 'domains': [], 'services': {'192.168.0.40/0/arp/': {'mac': '00:11:32:5b:8b:b9', 'macDateAdded': '2004-04-25', 'macVendor': 'Synology Incorporated', 'service.address': '192.168.0.40', 'service.id': '68af77fb-c89b-4190-ba4e-528d4bfb4eaf', 'service.port': '0', 'service.transport': 'arp', 'source': 'arp', 'ts': '1583271140'},
-...
-more asset/service info
-...
+(runZero-scripts) tyler:runZero-scripts/ (main✗) $ python3 export/run.py                                                                                                                                      [10:28:08]
+Found 50 total assets alive
+Found 1 assets with the MAC address A0:5C:D5:E7:C1:11
+Found 1 assets with the IP address 192.168.86.250
+Found 7 assets with the name TYLERS-PHONE
 ```
--- export/run.py
+++ export/run.py
@@ -1,49 +1,28 @@
 import requests
 import os
-import urllib.parse
 
 RUNZERO_EXPORT_TOKEN = os.environ["RUNZERO_EXPORT_TOKEN"]
 HEADERS = {"Authorization": f"Bearer {RUNZERO_EXPORT_TOKEN}"}
 BASE_URL = "https://console.runZero.com/api/v1.0"
 
+MAC = "A0:5C:D5:E7:C1:11"
+IP = "192.168.86.250"
+NAME = "TYLERS-PHONE"
 
 def main():
-    searches = [
-        {"type": "assets", "search": "_asset.protocol:tls"},
-        {"type": "assets", "search": "_asset.protocol:ntp"},
-        {
-            "type": "services",
-            "search": "_asset.protocol:ntp and protocol:ntp and has:ntp.skew",
-        },
-        {"type": "asset", "search": "_asset.protocol:tls AND tls.notAfterTS:<now"},
-        {"type": "asset", "search": 'protocol:="mountd" and nfs.allowed:"%=*"'},
-        {"type": "asset", "search": "_asset.protocol:tls AND tls.notAfterTS:<6weeks"},
-        {
-            "type": "services",
-            "search": "_asset.protocol:ntp and protocol:ntp and has:ntp.skew",
-        },
-        {
-            "type": "services",
-            "search": '(_asset.protocol:http AND not _asset.protocol:tls) AND ( html.inputs:"password:" OR last.html.inputs:"password:" OR has:http.head.wwwAuthenticate OR has:last.http.head.wwwAuthenticate)',
-        },
-        {
-            "type": "services",
-            "search": "_asset.protocol:kerberos and protocol:kerberos and kerberos.errorCode:68 and os:windows",
-        },
-        {
-            "type": "services",
-            "search": '_asset.protocol:ldap and protocol:ldap and (has:"ldap.isSynchronized" or has:"ldap.domainControllerFunctionality") and type:server and os:windows',
-        },
-    ]
-
-    for search in searches:
-        if search["type"] == "assets":
-            url = BASE_URL + "/export/org/assets.json?"
-        else:
-            url = BASE_URL + "/export/org/services.json?"
-        data = requests.get(url, headers=HEADERS, params={"search": search["search"]})
-        print("Result count: " + str(len(data.json())))
-        print(data.json())
+    url = BASE_URL + '/export/org/assets.json'
+    everything = requests.get(url, headers=HEADERS, params={
+        "search": "alive:t"})
+    print(f"Found {len(everything.json())} total assets alive")
+    mac_results = requests.get(url, headers=HEADERS, params={
+                        "search": f"mac:{MAC}"})
+    print(f"Found {len(mac_results.json())} assets with the MAC address {MAC}")
+    ip_results = requests.get(url, headers=HEADERS, params={
+        "search": f"address:{IP}"})
+    print(f"Found {len(ip_results.json())} assets with the IP address {IP}")
+    name_results = requests.get(url, headers=HEADERS, params={
+        "search": f"name:{NAME}"})
+    print(f"Found {len(name_results.json())} assets with the name {NAME}")
 
 
 if __name__ == "__main__":
