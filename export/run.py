5db8a6235d2f3d0148c7959880c5c013dec467d4
Tyler Diderich <tylerdiderich@gmail.com>
2023-04-28 08:12:08 -0500

    formatting

--- export/run.py
+++ export/run.py
@@ -11,15 +11,19 @@ NAME = "TYLERS-PHONE"
 
 def main():
     url = BASE_URL + '/export/org/assets.json'
+    
     everything = requests.get(url, headers=HEADERS, params={
         "search": "alive:t"})
     print(f"Found {len(everything.json())} total assets alive")
+    
     mac_results = requests.get(url, headers=HEADERS, params={
                         "search": f"alive:t mac:{MAC}"})
     print(f"Found {len(mac_results.json())} assets with the MAC address {MAC}")
+    
     ip_results = requests.get(url, headers=HEADERS, params={
         "search": f"alive:t address:{IP}"})
     print(f"Found {len(ip_results.json())} assets with the IP address {IP}")
+    
     name_results = requests.get(url, headers=HEADERS, params={
         "search": f"alive:t name:{NAME}"})
     print(f"Found {len(name_results.json())} assets with the name {NAME}")
