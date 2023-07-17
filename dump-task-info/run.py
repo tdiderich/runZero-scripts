eadef827014b4546f169598f493b0c253898c8aa
Tyler Diderich <tylerdiderich@gmail.com>
2023-07-17 10:07:32 -0500

    add time taken to task CSV

--- dump-task-info/run.py
+++ dump-task-info/run.py
@@ -4,12 +4,12 @@ import json
 import csv
 
 # AUTH
-RUNZERO_ORG_TOKEN = os.environ["RUNZERO_ORG_TOKEN"]
-HEADERS = {"Authorization": f"Bearer {RUNZERO_ORG_TOKEN}"}
-BASE_URL = "https://console.runZero.com/api/v1.0"
+RUNZERO_ORG_TOKEN = os.environ['RUNZERO_ORG_TOKEN']
+HEADERS = {'Authorization': f'Bearer {RUNZERO_ORG_TOKEN}'}
+BASE_URL = 'https://console.runZero.com/api/v1.0'
 
 # UPDATE IF NEEDED
-FIELDS = ['id', 'name', 'template_id']
+FIELDS = ['id', 'name', 'template_id', 'parent_id', 'time_taken', 'time_taken_minutes', 'time_taken_hours']
 
 def write_to_csv(output: dict, filename: str, fieldnames: list):
     file = open(filename, 'w')
@@ -22,9 +22,9 @@ def write_to_csv(output: dict, filename: str, fieldnames: list):
     file.close()
 
 def get_tasks():
-    url = BASE_URL + "/org/tasks"
+    url = BASE_URL + '/org/tasks'
     tasks = requests.get(
-        url, headers=HEADERS, params={"search": "recur:t type:scan"}
+        url, headers=HEADERS, params={'search': 'type:scan status:processed'}
     )
     return tasks.json()
 
@@ -34,8 +34,13 @@ def main():
     csv_out = []
     if len(tasks) > 0:
         for t in tasks:
+            finished = t['updated_at']
+            started = t['start_time']
+            t['time_taken_seconds'] = finished - started
+            t['time_taken_minutes'] = (finished - started) / 60
+            t['time_taken_hours'] = (finished - started) / 3600
             csv_out.append(dict((k, t[k]) for k in FIELDS if k in t))
-    print(csv_out)
+
     write_to_csv(
         output=csv_out,
         filename='tasks.csv',
@@ -44,5 +49,5 @@ def main():
 
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
