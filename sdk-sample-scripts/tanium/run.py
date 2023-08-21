f0b746b5a97787d621a6b71293984b2125824dab
Tyler Diderich <tylerdiderich@gmail.com>
2023-08-21 13:28:46 -0500

    tanium query logic

--- sdk-sample-scripts/tanium/run.py
+++ sdk-sample-scripts/tanium/run.py
@@ -6,39 +6,41 @@ TANIUM_URL = os.environ["TANIUM_URL"]
 TANIUM_TOKEN = os.environ["TANIUM_TOKEN"]
 
 
-query = """query getEndpoints($first: Int, $after: Cursor) {
-  endpoints(first: $first, after: $after) {
-    edges {
-      node {
-        computerID
-        name
-        serialNumber
-        ipAddress
-      }
-    }
-    pageInfo {
-      hasNextPage
-      endCursor
-      startCursor
-    }
-    totalRecords
-  }
-}"""
-
-
 def get_endpoints():
+    query = """query getEndpoints($first: Int, $after: Cursor) {
+    endpoints(first: $first, after: $after) {
+        edges {
+        node {
+            computerID
+            name
+            serialNumber
+            ipAddress
+        }
+        }
+        pageInfo {
+        hasNextPage
+        endCursor
+        startCursor
+        }
+        totalRecords
+    }
+    }"""
     cursor = None
     hasNextPage = True
     endpoints = []
     while hasNextPage:
+
+        # set cursor if it exists (all but the first query)
         if cursor:
             variables = {"first": 100, "after": cursor}
         else:
             variables = {"first": 100}
 
+        # get endpoints
         data = requests.post(TANIUM_URL + "/plugin/products/gateway/graphql",
                              headers={"Content-Type": "application/json", "session": TANIUM_TOKEN}, json={'query': query, 'variables': variables})
-        print(json.dumps(data.json(), indent=4))
+
+        # grab data from the response
         endpoints.extend(data.json()["data"]["endpoints"]["edges"])
         hasNextPage = data.json(
         )["data"]["endpoints"]["pageInfo"]["hasNextPage"]
